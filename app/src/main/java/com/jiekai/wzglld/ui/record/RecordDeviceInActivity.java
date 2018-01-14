f681fff6288bd1551e508a0252c5ddddb1555f85
liuhaipeng <haipengc51@126.com>
2018-01-14 22:49:28 +0800

    出库记录查询

---
+++ app/src/main/java/com/jiekai/wzglld/ui/record/RecordDeviceInActivity.java
@@ -0,0 +1,30 @@
+package com.jiekai.wzglld.ui.record;
+
+import com.jiekai.wzglld.R;
+import com.jiekai.wzglld.test.NFCBaseActivity;
+
+/**
+ * Created by laowu on 2018/1/14.
+ */
+
+public class RecordDeviceInActivity extends NFCBaseActivity {
+    @Override
+    public void getNfcData(String nfcString) {
+        setContentView(R.layout.activity_record_device_in);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initOperation() {
+
+    }
+}
